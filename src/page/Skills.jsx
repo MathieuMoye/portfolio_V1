29dc67f1ac89276ba91e339ebecc38ba3d982010
MathieuMoye <mathieu.afev66@gmail.com>
2024-04-08 14:14:07 +0200

    mise en place de la liste des skills

--- src/page/Skills.jsx
+++ src/page/Skills.jsx
@@ -1,11 +1,23 @@
 import Header from '../components/layout/Header.jsx';
+import '../assets/styles/skills.css'
+import { skills } from '../assets/json/skills';
 
 const Skills = () =>{
     return (
         <>
             <Header />
-            <div>
-                <h2>Skills</h2>
+            <div className='skills-container'>
+                <h2 className='skills-title'>Skills</h2>
+                <ul className='skills-list-container'>
+                    {
+                        skills.map((skill) => {
+                            return (
+                             <li className='skills-list'>{skill.name}<img className='skills-img' src={skill.img}></img></li>   
+                            )
+                            
+                        })
+                    }
+                </ul>
             </div>
         </>
             
